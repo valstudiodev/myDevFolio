47c520d756b6686ff6ac01824e295098e5a3bcb1
ukr23can <tkachenko6591@gmail.com>
2026-03-30 22:05:31 -0300

    upgrate pro version

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,8 @@ export default defineConfig({
 
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        works: resolve(__dirname, 'works.html')
       },
       output: {
         assetFileNames: 'assets/[name].[hash].[ext]',
